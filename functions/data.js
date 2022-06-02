73c4d47ec07a29eefeb7e138e838ab73406d8cf9
Shreyas Parbat <shreyasp.2016@sis.smu.edu.sg>
2022-06-02 20:29:58 +0800

    added readData route

--- functions/data.js
+++ functions/data.js
@@ -1,4 +1,25 @@
-// const functions = require("firebase-functions");
+const functions = require("firebase-functions");
+const { getFirestore } = require("firebase-admin/firestore");
+const cors = require("cors")({ origin: true });
 
-// Create a new view
-// exports.readMau = functions.https.onRequest((req, res) => {});
+// Get db instance
+const db = getFirestore();
+
+// Read all charts (to be broken up in future iterations)
+exports.readData = functions.https.onRequest(async (req, res) => {
+  cors(req, res, async () => {
+    try {
+      // Get snapshot of all docs in data collection
+      const snapshot = await db.collection("data").get();
+
+      // Send all docs along with ID
+      res.send(
+        snapshot.docs.map((doc) => ({ id: doc.id, data: doc.data().data }))
+      );
+    } catch (e) {
+      res.status(500).send({
+        error: e.stack,
+      });
+    }
+  });
+});
